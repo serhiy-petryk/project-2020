ce52c9de40d6b9e5455df042e191123845992c1a
serhiy-petryk <sergei_petrik@yahoo.com>
2021-06-08 16:33:34 +0300

    added WpfSpLib and DGView.2.0 projects

---
+++ DG.3.1/WpfSpLib/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfSpLib
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+        }
+    }
+}
