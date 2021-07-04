0e6745c396cf79e3281736d1cee26b89f8007cfb
serhiy-petryk <sergei_petrik@yahoo.com>
2021-07-04 12:48:29 +0300

    x

--- DG.3.1/WpfSpLib/Common/BindingProxy.cs
+++ DG.3.1/WpfSpLib/Common/BindingProxy.cs
@@ -67,32 +67,4 @@ namespace WpfSpLib.Common
         }
         #endregion
     }
-
-    //==========================================================================
-    public class DynamicBinding : Freezable
-    {
-        public DynamicBinding()
-        {
-            var dynamicResourceExtension = new DynamicResourceExtension(string.Empty);
-            Dummy = dynamicResourceExtension.ProvideValue(null);
-        }
-
-        protected override Freezable CreateInstanceCore() => new DynamicBinding();
-
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
-            nameof(Value), typeof(object), typeof(DynamicBinding), new FrameworkPropertyMetadata(default));
-        public object Value
-        {
-            get => GetValue(ValueProperty);
-            set => SetValue(ValueProperty, value);
-        }
-
-        private static readonly DependencyProperty DummyProperty = DependencyProperty.Register(
-            nameof(Dummy), typeof(object), typeof(DynamicBinding), new FrameworkPropertyMetadata(default));
-        private object Dummy
-        {
-            get => GetValue(DummyProperty);
-            set => SetValue(DummyProperty, value);
-        }
-    }
 }
