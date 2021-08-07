1badd89b4300a37b8a103e296b3986d8deafc030
serhiy-petryk <sergei_petrik@yahoo.com>
2021-08-08 00:00:20 +0300

    x

--- DG.3.1/DGView.2.0/Helpers/Converters.cs
+++ DG.3.1/DGView.2.0/Helpers/Converters.cs
@@ -5,9 +5,9 @@ using System.Windows.Data;
 
 namespace DGView.Helpers
 {
-    public class OpacityForDataGridRowHeader : DependencyObject, IValueConverter
+    public class OpacityForDataGridRowHeaderConverter : DependencyObject, IValueConverter
     {
-        public static OpacityForDataGridRowHeader Instance = new OpacityForDataGridRowHeader();
+        public static OpacityForDataGridRowHeaderConverter Instance = new OpacityForDataGridRowHeaderConverter();
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             // value.GetType().Name == "NamedObject" => new row ({{NewItemPlaceholder}}) in DataGrid
