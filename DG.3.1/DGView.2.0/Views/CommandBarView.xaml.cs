ea2c8396c91e7f39e270c4428f6098c168f8230b
serhiy-petryk <sergei_petrik@yahoo.com>
2021-08-22 11:23:32 +0300

    changed DataContext of CommandBar

--- DG.3.1/DGView.2.0/Views/CommandBarView.xaml.cs
+++ DG.3.1/DGView.2.0/Views/CommandBarView.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.ComponentModel;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using DGView.ViewModels;
 using WpfSpLib.Common;
 
 namespace DGView.Views
@@ -10,40 +8,28 @@ namespace DGView.Views
     /// <summary>
     /// Interaction logic for CommandBarView.xaml
     /// </summary>
-    public partial class CommandBarView : UserControl, INotifyPropertyChanged
+    public partial class CommandBarView : UserControl// , INotifyPropertyChanged
     {
-        public static readonly DependencyProperty DGViewModelProperty = DependencyProperty.Register("DGViewModel", typeof(DataGridViewModel), typeof(CommandBarView), new FrameworkPropertyMetadata(null));
-        public DataGridViewModel DGViewModel
-        {
-            get => (DataGridViewModel)GetValue(DGViewModelProperty);
-            set => SetValue(DGViewModelProperty, value);
-        }
-        public DataGrid DGControl => DGViewModel?.DGControl;
-
         public CommandBarView()
         {
             InitializeComponent();
-            DataContext = this;
+        }
+
+        private void CommandBarView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var dgv = this.GetVisualParents().OfType<DataGridView>().FirstOrDefault();
+            DataContext = dgv?.ViewModel;
         }
 
         #region ===========  INotifyPropertyChanged  ================
-
-        public event PropertyChangedEventHandler PropertyChanged;
+       /* public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertiesChanged(params string[] propertyNames)
         {
             foreach (var propertyName in propertyNames)
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+        }*/
         #endregion
 
-        private void CommandBarView_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            var dgv = this.GetVisualParents().OfType<DataGridView>().FirstOrDefault();
-            DGViewModel = dgv?.ViewModel;
-
-            OnPropertiesChanged(nameof(DGControl));
-        }
     }
 }
