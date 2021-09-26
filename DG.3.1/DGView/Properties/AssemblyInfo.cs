66ec2ff8daa104e544fbaa5c5a229104d5d3bd9d
serhiy-petryk <sergei_petrik@yahoo.com>
2021-09-27 00:39:37 +0300

    renamed DGView project

--- DG.3.1/DGView/Properties/AssemblyInfo.cs
+++ DG.3.1/DGView/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DGView.2.0")]
+[assembly: AssemblyTitle("DGView")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DGView.2.0")]
+[assembly: AssemblyProduct("DGView")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
