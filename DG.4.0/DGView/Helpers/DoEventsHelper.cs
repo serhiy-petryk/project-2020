46dc70a852c81ee00db458bd72703adb741b25f5
serhiy-petryk <sergei_petrik@yahoo.com>
2021-10-08 01:25:10 +0300

    added DoEventsHelper

---
+++ DG.4.0/DGView/Helpers/DoEventsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Threading;
+
+namespace DGView.Helpers
+{
+    internal class DoEventsHelper
+    {
+        private static readonly DispatcherOperationCallback _exitFrameCallback = new DispatcherOperationCallback(ExitFrame);
+        internal static void DoEvents()
+        {
+            DispatcherFrame nestedFrame = new DispatcherFrame();
+            DispatcherOperation exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, _exitFrameCallback, nestedFrame);
+            Dispatcher.PushFrame(nestedFrame);
+
+            if (exitOperation.Status != DispatcherOperationStatus.Completed)
+            {
+                exitOperation.Abort();
+            }
+        }
+        private static object ExitFrame(object state)
+        {
+            DispatcherFrame frame = state as DispatcherFrame;
+            frame.Continue = false;
+            return null;
+        }
+
+    }
+}
