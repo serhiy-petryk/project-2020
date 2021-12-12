61c3daa608ee5b0960bb77d2f9a4b9b0f97ae986
serhiy-petryk <sergei_petrik@yahoo.com>
2021-12-12 02:14:22 +0200

    Update VCubeMastAccount.cs

--- DG.4.0/DGCore/Models/VCubeMastAccount.cs
+++ DG.4.0/DGCore/Models/VCubeMastAccount.cs
@@ -1,13 +1,50 @@
 ﻿using System;
-using DGCore.Common;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DGCore.DB;
 
 namespace Models
 {
     public class VCubeMastAccount
     {
+        private static Dictionary<string, VCubeMastAltAcc> Data;
+        static VCubeMastAccount()
+        {
+            // var a = new BO_LookupTableAttribute("SqlClient;initial catalog=dbOneSAP_DW;Pooling=false;Data Source=localhost;Integrated Security=SSPI;Connection Timeout=300", "select * from vcube_mastaltacc", "ALTACC");
+            //DGCore.DB.LookupTableHelper.InitLookupTableTypeConverter(typeof(VCubeMastAltAcc), a);
+
+            string conn = @"SqlClient;initial catalog=dbOneSAP_DW;Pooling=false;Data Source=localhost;Integrated Security=SSPI;Connection Timeout=300";
+            string sql = "select * from vcube_mastaltacc";
+            using (var cmd = new DbCmd(conn, sql))
+            {
+                var data = new Dictionary<string, VCubeMastAltAcc>();
+                cmd.Fill<string, VCubeMastAltAcc>(data, new Func<VCubeMastAltAcc, string>(x => x.ALTACC), null);
+                Data = data;
+                //var converter = new LookupTableTypeConverter<VCubeMastAltAcc, string>(data);
+                // typeof(VCubeMastAccount).GetProperty("ALTACC").Attributes.Add
+                // TypeDescriptor.AddAttributes(typeof(VCubeMastAltAcc), new TypeConverterAttribute(typeof(LookupTableTypeConverter<VCubeMastAltAcc, string>)));
+            }
+
+        }
+
         public string ACCOUNT { get; set; }
-        [BO_LookupTable("SqlClient;initial catalog=dbOneSAP_DW;Pooling=false;Data Source=localhost;Integrated Security=SSPI;Connection Timeout=300", "select * from vcube_mastaltacc", "ALTACC")]
-        public VCubeMastAltAcc ALTACC { get; set; }
+        private string _altAcc;
+        public string ALTACC
+        {
+            get => _altAcc;
+            set
+            {
+                _altAcc = value;
+            }
+        }
+
+        [Browsable(false)]
+        public VCubeMastAltAcc oALTACC
+        {
+            get => Data.ContainsKey(_altAcc) ? Data[_altAcc] : null;
+        }
+
+        public string ALTACC__AAA => oALTACC?.SHORTNAME;
         public string ACCOUNT_GROUP { get; set; }
         public bool? BALANCE_SHEET { get; set; }
         public string SHORT_TEXT { get; set; }
