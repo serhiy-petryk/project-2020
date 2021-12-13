2c0e92b83f89c286d15454032ce1c5d882cdd658
serhiy-petryk <sergei_petrik@yahoo.com>
2021-12-13 02:39:32 +0200

    refactored DynamicType.cs in DGWnd.3.1 project

--- DG.3.1/DGCore/PD/DynamicType.cs
+++ DG.3.1/DGCore/PD/DynamicType.cs
@@ -33,12 +33,12 @@ namespace DGCore.PD
             //      ModuleBuilder moduleBuilder = (ModuleBuilder)_dynamicAssembly.GetModule(moduleName);
 
             // Add new type to module
-            TypeBuilder typeBuilder = _moduleBuilder.DefineType("DynamicType_" + Utils.Tips.GetUniqueNumber().ToString(), TypeAttributes.Public);
+            TypeBuilder tb = _moduleBuilder.DefineType("DynamicType_" + Utils.Tips.GetUniqueNumber().ToString(), TypeAttributes.Public);
             Dictionary<string, FieldBuilder> fields = new Dictionary<string, FieldBuilder>();
             // Add fields
             for (int i = 0; i < propertyNames.Count; i++)
             {
-                FieldBuilder fieldBuilder = typeBuilder.DefineField(propertyNames[i], propertyTypes[i], FieldAttributes.Public);
+                FieldBuilder fieldBuilder = tb.DefineField(propertyNames[i], propertyTypes[i], FieldAttributes.Public);
                 fields.Add(propertyNames[i], fieldBuilder);
                 if (customAttributes != null && customAttributes.ContainsKey(propertyNames[i]))
                 {
@@ -54,21 +54,21 @@ namespace DGCore.PD
             // Create some methods if there is primary key
             if (primaryKey != null && primaryKey.Length > 0)
             {
-                typeBuilder.AddInterfaceImplementation(typeof(IComparable));
+                tb.AddInterfaceImplementation(typeof(IComparable));
                 // Prepare primary key fields array
                 FieldBuilder[] pkFields = new FieldBuilder[primaryKey.Length];
                 for (int i = 0; i < primaryKey.Length; i++) pkFields[i] = fields[primaryKey[i]];
 
-                BuildMethod_GetHashCode(typeBuilder, pkFields);
-                BuildMethod_ToString(typeBuilder, pkFields);
-                BuildMethod_Equals(typeBuilder, pkFields);
-                BuildMethod_CompareTo(typeBuilder, pkFields);
+                BuildMethod_GetHashCode(tb, pkFields);
+                BuildMethod_ToString(tb, pkFields);
+                BuildMethod_Equals(tb, pkFields);
+                BuildMethod_CompareTo(tb, pkFields);
                 if (primaryKey.Length > 1)
                 {// TO DO!!! build the function Get_PK(): Common.PrimaryKey is return value
                 }
             }
 
-            Type dynType = typeBuilder.CreateType();
+            Type dynType = tb.CreateType();
             if (File.Exists(moduleName))
                 File.Delete(moduleName);
 
@@ -94,13 +94,13 @@ namespace DGCore.PD
             //      ModuleBuilder moduleBuilder = (ModuleBuilder)_dynamicAssembly.GetModule(moduleName);
 
             // Add new type to module
-            TypeBuilder typeBuilder = _moduleBuilder.DefineType("DynamicType_" + Utils.Tips.GetUniqueNumber().ToString(), TypeAttributes.Public);
+            TypeBuilder tb = _moduleBuilder.DefineType("DynamicType_" + Utils.Tips.GetUniqueNumber().ToString(), TypeAttributes.Public);
             Dictionary<string, FieldBuilder> fields = new Dictionary<string, FieldBuilder>();
             Dictionary<string, PropertyBuilder> properties = new Dictionary<string, PropertyBuilder>();
             // Add fields
             for (int i = 0; i < propertyNames.Count; i++)
             {
-                var builders = GetDynamicPropertyBuilder(typeBuilder, propertyNames[i], propertyTypes[i]);
+                var builders = GetDynamicPropertyBuilder(tb, propertyNames[i], propertyTypes[i]);
                 fields.Add($"{FIELD_PREFIX}{propertyNames[i]}", builders.Item2);
                 properties.Add(propertyNames[i], builders.Item1);
 
@@ -119,22 +119,22 @@ namespace DGCore.PD
             // Create some methods if there is primary key
             if (primaryKey != null && primaryKey.Length > 0)
             {
-                typeBuilder.AddInterfaceImplementation(typeof(IComparable));
+                tb.AddInterfaceImplementation(typeof(IComparable));
                 // Prepare primary key fields array
                 FieldBuilder[] pkFields = new FieldBuilder[primaryKey.Length];
                 for (int i = 0; i < primaryKey.Length; i++)
                     pkFields[i] = fields[$"{FIELD_PREFIX}{primaryKey[i]}"];
 
-                BuildMethod_GetHashCode(typeBuilder, pkFields);
-                BuildMethod_ToString(typeBuilder, pkFields);
-                BuildMethod_Equals(typeBuilder, pkFields);
-                BuildMethod_CompareTo(typeBuilder, pkFields);
+                BuildMethod_GetHashCode(tb, pkFields);
+                BuildMethod_ToString(tb, pkFields);
+                BuildMethod_Equals(tb, pkFields);
+                BuildMethod_CompareTo(tb, pkFields);
                 if (primaryKey.Length > 1)
                 {// TO DO!!! build the function Get_PK(): Common.PrimaryKey is return value
                 }
             }
 
-            Type dynType = typeBuilder.CreateType();
+            Type dynType = tb.CreateType();
             if (File.Exists(moduleName))
                 File.Delete(moduleName);
 
@@ -163,9 +163,6 @@ namespace DGCore.PD
                 // typeBuilder.DefineField($"{FIELD_PREFIX}{propertyName}", propertyType, FieldAttributes.Private);
                 typeBuilder.DefineField($"{FIELD_PREFIX}{propertyName}", propertyType, FieldAttributes.Public);
 
-            PropertyBuilder propertyBuilder =
-                typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
-
             //Getter
             MethodBuilder methodGetBuilder = typeBuilder.DefineMethod($"get_{propertyName}",
                 MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyType,
@@ -187,6 +184,8 @@ namespace DGCore.PD
             setIL.Emit(OpCodes.Stfld, fieldBuilder);
             setIL.Emit(OpCodes.Ret);
 
+            PropertyBuilder propertyBuilder =
+                typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
             propertyBuilder.SetGetMethod(methodGetBuilder);
             propertyBuilder.SetSetMethod(methodSetBuilder);
 
