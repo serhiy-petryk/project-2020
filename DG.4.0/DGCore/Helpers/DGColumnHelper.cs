8056dbda5dfe29b7e4c67d2134d6dcdc1c253b70
serhiy-petryk <sergei_petrik@yahoo.com>
2021-12-19 01:24:25 +0200

    Update DGColumnHelper.cs

--- DG.4.0/DGCore/Helpers/DGColumnHelper.cs
+++ DG.4.0/DGCore/Helpers/DGColumnHelper.cs
@@ -1,24 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
-using DGCore.Common;
-using DGCore.DGVList;
 using DGCore.PD;
 
 namespace DGCore.Helpers
 {
     public class DGColumnHelper
     {
-        public static DGColumnHelper GetColumnHelper(string propertyName, PropertyDescriptorCollection pdc)
-        {
-            if (propertyName == Constants.GroupItemCountColumnName)
-                return new DGColumnHelper(new PropertyDescriptorForGroupItemCount());
-            var propertyDescriptor = pdc[propertyName];
-            return new DGColumnHelper(propertyDescriptor);
-        }
-
-        private int? _groupLevel = null;
-        private Func<object, object> _getter;
-
         public string Name { get; }
         public string DisplayName { get; }
         public string Format { get; }
@@ -26,6 +13,7 @@ namespace DGCore.Helpers
 
         public Type ValueType { get; }
 
+        private Func<object, object> _getter;
         // For common columns
         public DGColumnHelper(PropertyDescriptor pd)
         {
