aa3bc78acd8549146353def15c6aef2ed163b92d
serhiy-petryk <sergei_petrik@yahoo.com>
2021-12-28 08:37:03 +0200

    fixed width of ThemeSelector

--- DG.4.0/WpfSpLib/Helpers/ColorThemeSupportHelper.cs
+++ DG.4.0/WpfSpLib/Helpers/ColorThemeSupportHelper.cs
@@ -37,7 +37,7 @@ namespace WpfSpLib.Helpers
             var mwiChild = new MwiChild
             {
                 Content = themeSelector,
-                Width = 770,
+                Width = 780,
                 Height = 600,
                 MinWidth = 700,
                 MinHeight = 500,
