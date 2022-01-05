e11d677a7c742e91fdde863ab8d238bc57c53b88
serhiy-petryk <sergei_petrik@yahoo.com>
2022-01-05 19:58:40 +0200

    refactored MonochromeRadioButton style in WpfSpLib

--- DG.4.0/WpfSpLibDemo/TestViews/FormControlStylesTests.xaml.cs
+++ DG.4.0/WpfSpLibDemo/TestViews/FormControlStylesTests.xaml.cs
@@ -1,15 +1,24 @@
 ﻿using System.Windows;
+using System.Windows.Media;
+using WpfSpLib.Helpers;
+using WpfSpLib.Themes;
 
 namespace WpfSpLibDemo.TestViews
 {
     /// <summary>
     /// Interaction logic for FormControlStylesTests.xaml
     /// </summary>
-    public partial class FormControlStylesTests : Window
+    public partial class FormControlStylesTests : Window, IColorThemeSupport
     {
         public FormControlStylesTests()
         {
             InitializeComponent();
         }
+
+        public MwiThemeInfo Theme { get; set; } 
+        public Color? ThemeColor { get; set; }
+        public MwiThemeInfo ActualTheme { get; }
+        public Color ActualThemeColor => (Color) ColorConverter.ConvertFromString("#FFF5FAFF");
+        public IColorThemeSupport ColorThemeParent { get; }
     }
 }
