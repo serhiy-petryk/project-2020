1c02ed1460c189373b82d74929d771b7f7e2677d
serhiy-petryk <sergei_petrik@yahoo.com>
2022-01-08 22:40:52 +0200

    WiP DGSaveSettingView

--- DG.4.0/DGView/Views/DGSaveSettingView.xaml.cs
+++ DG.4.0/DGView/Views/DGSaveSettingView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
@@ -6,9 +7,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Threading;
+using DGCore.Common;
 using DGCore.UserSettings;
+using DGView.Helpers;
 using DGView.ViewModels;
 using WpfSpLib.Common;
+using WpfSpLib.Controls;
 using WpfSpLib.Helpers;
 
 namespace DGView.Views
@@ -35,9 +39,10 @@ namespace DGView.Views
             Dispatcher.BeginInvoke(new Action(() => { NewSettingName.Focus(); }), DispatcherPriority.Background);
             Unloaded += OnUnloaded;
 
-            CmdDeleteRow = new RelayCommand(cmdDeleteRow);
-            CmdSetSetting = new RelayCommand(cmdSetSetting);
-
+            CmdDeleteRow = new RelayCommand(cmdDeleteRow, o => DataGrid.SelectedItems.Count == 1);
+            CmdSaveChanges = new RelayCommand(cmdSaveChanges, o => ((List<UserSettingsDbObject>)DataGrid.ItemsSource).Any(o1 => o1.IsDeleted));
+            CmdSaveNewSetting = new RelayCommand(cmdSaveNewSetting, o => !string.IsNullOrEmpty(NewSettingName.Text));
+            CmdSetSetting = new RelayCommand(cmdSetSetting, o => DataGrid.SelectedItems.Count == 1);
         }
 
         private void OnUnloaded(object sender, RoutedEventArgs e)
@@ -64,6 +69,8 @@ namespace DGView.Views
 
         #region =========  Commands  ===========
         public RelayCommand CmdDeleteRow { get; }
+        public RelayCommand CmdSaveChanges { get; }
+        public RelayCommand CmdSaveNewSetting { get; }
         public RelayCommand CmdSetSetting { get; }
         private void cmdDeleteRow(object p)
         {
@@ -74,6 +81,47 @@ namespace DGView.Views
                 item.OnPropertiesChanged("IsDeleted");
             }
         }
+        private void cmdSaveChanges(object p)
+        {
+            /*var result = DGCore.UserSettings.UserSettingsUtils.SaveChangedSettings((List<DGCore.UserSettings.UserSettingsDbObject>)dataGridView1.DataSource, _properties);
+            if (result == 0)
+            {
+                MessageBox.Show(@"Не було змінено жодного налаштування", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (result > 0)
+            {
+                MessageBox.Show($@"Було записано {result} змінених налаштуваннь", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Flag = true;
+                var cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+                var data = dataGridView1.DataSource;
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = data;
+            }
+            else
+            {
+                MessageBox.Show(@"Помилка! Було спроба записати налаштування, для якого Ви не маєте права це робити", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+        }
+
+        private void cmdSaveNewSetting(object p)
+        {
+            var settingId = NewSettingName.Text;
+            var allowEdit = cbAllowEditToOthers.IsChecked ?? false;
+            var allowView = cbAllowViewToOthers.IsChecked ?? false;
+
+            if (string.IsNullOrEmpty(settingId))
+            {
+                Shared.ShowMessage(@"Налаштування не може бути пустим", "", Enums.MessageBoxButtons.OK, Enums.MessageBoxIcon.Error);
+                return;
+            }
+
+            if (UserSettingsUtils.SaveNewSetting(_viewModel, settingId, allowView, allowEdit))
+            {
+                _viewModel.cmdSetSetting(settingId);
+                ApplicationCommands.Close.Execute(null, null);
+            }
+        }
+
         private void cmdSetSetting(object p)
         {
             if (DataGrid.SelectedItems.Count == 1)
