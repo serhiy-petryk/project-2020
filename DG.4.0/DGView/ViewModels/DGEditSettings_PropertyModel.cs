32585250dbac36c010dcf24b13bfbba16b580df7
serhiy-petryk <sergei_petrik@yahoo.com>
2022-01-24 20:53:03 +0200

    WiP DGEditSettings

---
+++ DG.4.0/DGView/ViewModels/DGEditSettings_PropertyModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using DGCore.Common;
+using DGCore.PD;
+using DGCore.UserSettings;
+
+namespace DGView.ViewModels
+{
+    public class DGEditSettings_PropertyModel
+    {
+        [Browsable(false)]
+        public string Id { get; }
+        public string Name => Id.Replace(Constants.MDelimiter, Environment.NewLine);
+        public string Description { get; }
+        public string Format { get; set; }
+        public bool IsHidden { get; set; }
+        public ListSortDirection? IsGrouping { get; set; }
+
+        public DGEditSettings_PropertyModel(Column column, IMemberDescriptor descriptor)
+        {
+            Id = column.Id;
+            IsHidden = column.IsHidden;
+            Format = descriptor.Format;
+            // Description = descriptor.De
+        }
+    }
+}
--- DG.4.0/DGView/Views/DGEditSettingsView.xaml.cs
+++ DG.4.0/DGView/Views/DGEditSettingsView.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Threading;
+using DGCore.PD;
 using DGCore.UserSettings;
+using DGView.ViewModels;
 using WpfSpLib.Helpers;
 
 namespace DGView.Views
@@ -13,13 +19,13 @@ namespace DGView.Views
     /// </summary>
     public partial class DGEditSettingsView : UserControl
     {
-        public ObservableCollection<Column> PropertiesData { get; }
+        public ObservableCollection<DGEditSettings_PropertyModel> PropertiesData { get; }
 
-        public DGEditSettingsView(DGV settings)
+        public DGEditSettingsView(DGV settings, PropertyDescriptorCollection properties)
         {
             InitializeComponent();
             DataContext = this;
-            PropertiesData = new ObservableCollection<Column>(settings.AllColumns);
+            PropertiesData = new ObservableCollection<DGEditSettings_PropertyModel>(settings.AllColumns.Select(o => new DGEditSettings_PropertyModel(o, (IMemberDescriptor)properties[o.Id])));
         }
 
         #region =======  Drag/Drop event handlers ========
@@ -32,7 +38,7 @@ namespace DGView.Views
         {
             if (!DragDropHelper.Drag_Info.InsertIndex.HasValue || e.Effects != DragDropEffects.Copy) return;
 
-            var sourceData = ((e.Data.GetData(sender.GetType().Name) as object[]) ?? new object[0]).OfType<Column>().ToArray();
+            var sourceData = ((e.Data.GetData(sender.GetType().Name) as object[]) ?? new object[0]).OfType<DGEditSettings_PropertyModel>().ToArray();
             var insertIndex = DragDropHelper.Drag_Info.InsertIndex.Value + DragDropHelper.Drag_Info.FirstItemOffset;
             foreach (var item in sourceData)
             {
@@ -45,5 +51,24 @@ namespace DGView.Views
         }
 
         #endregion
+
+        private void DataGridCell_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var cell = sender as DataGridCell;
+            if (cell.IsReadOnly) return;
+
+            var toggleButtons = cell.GetVisualChildren().OfType<ToggleButton>().ToArray();
+            if (toggleButtons.Length == 1)
+            {
+                var dataGrid = cell.GetVisualParents().OfType<DataGrid>().FirstOrDefault();
+                if (dataGrid != null)
+                {
+                    dataGrid.BeginEdit();
+                    toggleButtons[0].SetCurrentValue(ToggleButton.IsCheckedProperty, !toggleButtons[0].IsChecked);
+                    dataGrid.CommitEdit();
+                    e.Handled = true;
+                }
+            }
+        }
     }
 }
