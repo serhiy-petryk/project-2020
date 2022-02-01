cd130b3aaab020f2fdfce4b479ac96721d7d6688
serhiy-petryk <sergei_petrik@yahoo.com>
2022-02-01 03:16:05 +0200

    added ItemType enum in PropertyGroupItem

--- DG.4.0/DGView/ViewModels/DGProperty_GroupItemModel.cs
+++ DG.4.0/DGView/ViewModels/DGProperty_GroupItemModel.cs
@@ -9,6 +9,7 @@ namespace DGView.ViewModels
 {
     public class PropertyGroupItem : INotifyPropertyChanged
     {
+        public enum ItemType { Root, Details, Group, Label, Sorting}
         public static IEnumerable<PropertyGroupItem> GetAllChildren(PropertyGroupItem current)
         {
             yield return current;
@@ -27,15 +28,16 @@ namespace DGView.ViewModels
             set
             {
                 _sortDirection = value;
-                if (Type == "Group" && !Equals(Item.GroupDirection, value))
+                if (Type == ItemType.Group && !Equals(Item.GroupDirection, value))
                     Item.GroupDirection = value;
                 OnPropertiesChanged(nameof(SortDirection));
             }
         }
 
-        public bool CanSort => Type == "Group" || Type == "Sort";
-        public string Name => Type == "Details" ? "Detail items" :(Item == null ? "Sortings:" : Item.Name);
-        public string Type => Parent == null ? "Root" : (Item == null ? ( Parent.Type == "Root" ? "Details" : "Label") : (Parent.Type == "Root" ? "Group" : "Sort"));
+        public bool CanSort => Type == ItemType.Group  || Type == ItemType.Sorting;
+        public string Name => Type == ItemType.Details  ? "Detail items" :(Item == null ? "Sortings:" : Item.Name);
+//        public string Type => Parent == null ? "Root" : (Item == null ? (Parent.Type == "Root" ? "Details" : "Label") : (Parent.Type == "Root" ? "Group" : "Sort"));
+        public ItemType Type => Parent == null ? ItemType.Root : (Item == null ? (Parent.Type == ItemType.Root  ? ItemType.Details : ItemType.Label) : (Parent.Type == ItemType.Root ? ItemType.Group : ItemType.Sorting));
         public ObservableCollection<PropertyGroupItem> Children { get; } = new ObservableCollection<PropertyGroupItem>();
         public PropertyGroupItem Root => Parent == null ? this : Parent.Root;
         public Color BaseColor
@@ -43,8 +45,8 @@ namespace DGView.ViewModels
             get
             {
 //                if (Type == "Details") return Color.FromArgb(0xFF, 0xF5, 0xFA, 0xFF);
-                if (Type == "Details") return Colors.White;
-                if (Type == "Group")
+                if (Type == ItemType.Details) return Colors.White;
+                if (Type == ItemType.Group)
                 {
                     var groupIndex = Root.Children.IndexOf(this) % (DGCore.Helpers.ColorInfo.GroupColors.Length - 1) + 1;
                     var groupColor = DGCore.Helpers.ColorInfo.GroupColors[groupIndex];
@@ -63,9 +65,9 @@ namespace DGView.ViewModels
 
         private void cmdClear(object p)
         {
-            if (Type == "Group")
+            if (Type == ItemType.Group)
                 Item.GroupDirection = null;
-            else if (Type == "Sort")
+            else if (Type == ItemType.Sorting)
                 Parent.Children.Remove(this);
         }
 
@@ -75,11 +77,11 @@ namespace DGView.ViewModels
             if (oldItem != null)
                 Children.Remove(oldItem);
             var newItem = new PropertyGroupItem { Parent = this, Item = item, SortDirection = sortDirection };
-            if (Children.Count > 0 && Children[Children.Count - 1].Type == "Details")
+            if (Children.Count > 0 && Children[Children.Count - 1].Type == ItemType.Details)
                 Children.Insert(Children.Count - 1, newItem);
             else
                 Children.Add(newItem);
-            if (newItem.Type == "Group" || newItem.Type == "Details")
+            if (newItem.Type == ItemType.Group || newItem.Type == ItemType.Details)
                 newItem.Children.Add(new PropertyGroupItem { Parent = newItem }); // Add "Sortings:" label
             return newItem;
         }
