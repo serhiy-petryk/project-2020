ae5e2a422afa60e158481b09503bbca144a306a9
serhiy-petryk <sergei_petrik@yahoo.com>
2022-02-14 21:40:17 +0200

    Update ItemsControlHelper.cs

--- DG.4.0/WpfSpLib/Helpers/ItemsControlHelper.cs
+++ DG.4.0/WpfSpLib/Helpers/ItemsControlHelper.cs
@@ -13,10 +13,10 @@ namespace WpfSpLib.Helpers
 {
     public static class ItemsControlHelper
     {
-        public static async Task AddOrReorderItem(this ItemsControl control, object item, int newIndex)
+        /*public static async Task AddOrReorderItems(this ItemsControl control, object[] item, int newIndex)
         {
-            var items = (IList)(control.ItemsSource ?? control.Items);
-            var oldIndex = items.IndexOf(item);
+            var itemsSource = (IList)(control.ItemsSource ?? control.Items);
+            var oldIndex = itemsSource.IndexOf(item);
             if (Equals(oldIndex, newIndex)) return;
 
             // To prevent flicker during drop item
@@ -30,13 +30,60 @@ namespace WpfSpLib.Helpers
                 element = control.ItemContainerGenerator.ContainerFromIndex(oldIndex) as FrameworkElement;
                 if (element != null)
                     await Task.WhenAll(AnimationHelper.GetHeightContentAnimations(element, false));
-                items.RemoveAt(oldIndex);
+                itemsSource.RemoveAt(oldIndex);
                 element?.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
             }
 
             if (item is TabItem tabItem)
                 ((TabControl)tabItem.Parent)?.Items.Remove(tabItem);
-            items.Insert(newIndex, item);
+            itemsSource.Insert(newIndex, item);
+
+            VisualHelper.DoEvents(DispatcherPriority.Render);
+
+            element = control.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+            if (element != null)
+            {
+                element.SetCurrentValueSmart(UIElement.OpacityProperty, 1.0);
+                await Task.WhenAll(AnimationHelper.GetHeightContentAnimations(element, true));
+                element.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
+            }
+
+            control.ItemContainerGenerator.StatusChanged -= OnItemContainerGeneratorStatusChanged;
+
+            void OnItemContainerGeneratorStatusChanged(object sender, EventArgs e)
+            {
+                var generator = (ItemContainerGenerator)sender;
+                if (generator.Status != GeneratorStatus.ContainersGenerated) return;
+                foreach (var item2 in generator.Items)
+                    if (generator.ContainerFromItem(item2) is FrameworkElement fe && fe.Opacity > double.Epsilon && fe.ActualHeight < double.Epsilon && fe.ActualWidth < double.Epsilon)
+                        fe.SetCurrentValueSmart(UIElement.OpacityProperty, 0.0);
+            }
+        }*/
+
+        public static async Task AddOrReorderItem(this ItemsControl control, object item, int newIndex)
+        {
+            var itemsSource = (IList)(control.ItemsSource ?? control.Items);
+            var oldIndex = itemsSource.IndexOf(item);
+            if (Equals(oldIndex, newIndex)) return;
+
+            // To prevent flicker during drop item
+            control.ItemContainerGenerator.StatusChanged += OnItemContainerGeneratorStatusChanged;
+
+            FrameworkElement element;
+            if (oldIndex >= 0)
+            {
+                // var visualItems = GetItemsHost(control).Children.OfType<FrameworkElement>();
+                // element = visualItems.FirstOrDefault(o => o.DataContext == item);
+                element = control.ItemContainerGenerator.ContainerFromIndex(oldIndex) as FrameworkElement;
+                if (element != null)
+                    await Task.WhenAll(AnimationHelper.GetHeightContentAnimations(element, false));
+                itemsSource.RemoveAt(oldIndex);
+                element?.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
+            }
+
+            if (item is TabItem tabItem)
+                ((TabControl)tabItem.Parent)?.Items.Remove(tabItem);
+            itemsSource.Insert(newIndex, item);
 
             VisualHelper.DoEvents(DispatcherPriority.Render);
 
