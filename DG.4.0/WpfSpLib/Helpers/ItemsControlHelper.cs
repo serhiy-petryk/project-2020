59bc711bac580471a60891700a748cc69cdc89df
serhiy-petryk <sergei_petrik@yahoo.com>
2022-02-16 21:10:26 +0200

    Update ItemsControlHelper.cs

--- DG.4.0/WpfSpLib/Helpers/ItemsControlHelper.cs
+++ DG.4.0/WpfSpLib/Helpers/ItemsControlHelper.cs
@@ -83,7 +83,6 @@ namespace WpfSpLib.Helpers
             {
                 if (control.ItemContainerGenerator.ContainerFromItem(item) is FrameworkElement itemVisual)
                 {
-                    // itemVisual.SetCurrentValueSmart(UIElement.OpacityProperty, 1.0);
                     animations.AddRange(AnimationHelper.GetHeightContentAnimations(itemVisual, true));
                     elements.Add(itemVisual);
                 }
@@ -116,126 +115,6 @@ namespace WpfSpLib.Helpers
 
                     fe.Dispatcher.BeginInvoke(new Action(() => fe.LayoutTransform = Transform.Identity), DispatcherPriority.Render);
                 }
-            }
-        }
-
-
-        public static async Task xxAddOrReorderItems(this ItemsControl control, object[] sourceData, int insertIndex)
-        {
-            var items = GetItemsHost(control).Children.OfType<FrameworkElement>().ToArray();
-            var itemsSource = (IList)(control.ItemsSource ?? control.Items);
-
-            // To prevent flicker during drop item
-            control.ItemContainerGenerator.StatusChanged += OnItemContainerGeneratorStatusChanged;
-
-            var animations = new List<Task>();
-            var elements = new List<FrameworkElement>();
-            foreach (var item in sourceData)
-            {
-                var oldIndex = itemsSource.IndexOf(item);
-                if (oldIndex >= 0)
-                {
-                    var removingElement = items.FirstOrDefault(o => o.DataContext == item);
-                    if (removingElement != null)
-                    {
-                        elements.Add(removingElement);
-                        animations.AddRange(AnimationHelper.GetHeightContentAnimations(removingElement, false));
-                    }
-                }
-            }
-
-            await Task.WhenAll(animations.ToArray());
-            foreach (var element in elements)
-                element.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
-
-            foreach (var item in sourceData)
-            {
-                var oldIndex = itemsSource.IndexOf(item);
-                if (oldIndex >= 0)
-                {
-                    if (oldIndex < insertIndex) insertIndex--;
-                    itemsSource.RemoveAt(oldIndex);
-                }
-
-                if (item is TabItem tabItem)
-                    ((TabControl)tabItem.Parent)?.Items.Remove(tabItem);
-                itemsSource.Insert(insertIndex++, item);
-            }
-
-            VisualHelper.DoEvents(DispatcherPriority.Background);
-
-            animations.Clear();
-            elements.Clear();
-            foreach (var item in sourceData)
-            {
-                if (control.ItemContainerGenerator.ContainerFromItem(item) is FrameworkElement itemVisual)
-                {
-                    // itemVisual.SetCurrentValueSmart(UIElement.OpacityProperty, 1.0);
-                    animations.AddRange(AnimationHelper.GetHeightContentAnimations(itemVisual, true));
-                    elements.Add(itemVisual);
-                }
-            }
-
-            await Task.WhenAll(animations);
-            foreach (var element in elements)
-                element.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
-
-            control.ItemContainerGenerator.StatusChanged -= OnItemContainerGeneratorStatusChanged;
-
-            void OnItemContainerGeneratorStatusChanged(object sender, EventArgs e)
-            {
-                var generator = (ItemContainerGenerator)sender;
-                if (generator.Status != GeneratorStatus.ContainersGenerated) return;
-                foreach (var item2 in generator.Items)
-                    if (generator.ContainerFromItem(item2) is FrameworkElement fe && fe.Opacity > double.Epsilon && fe.ActualHeight < double.Epsilon && fe.ActualWidth < double.Epsilon)
-                        fe.SetCurrentValueSmart(UIElement.OpacityProperty, 0.0);
-            }
-        }
-
-        public static async Task AddOrReorderItem(this ItemsControl control, object item, int newIndex)
-        {
-            var itemsSource = (IList)(control.ItemsSource ?? control.Items);
-            var oldIndex = itemsSource.IndexOf(item);
-            if (Equals(oldIndex, newIndex)) return;
-
-            // To prevent flicker during drop item
-            control.ItemContainerGenerator.StatusChanged += OnItemContainerGeneratorStatusChanged;
-
-            FrameworkElement element;
-            if (oldIndex >= 0)
-            {
-                // var visualItems = GetItemsHost(control).Children.OfType<FrameworkElement>();
-                // element = visualItems.FirstOrDefault(o => o.DataContext == item);
-                element = control.ItemContainerGenerator.ContainerFromIndex(oldIndex) as FrameworkElement;
-                if (element != null)
-                    await Task.WhenAll(AnimationHelper.GetHeightContentAnimations(element, false));
-                itemsSource.RemoveAt(oldIndex);
-                element?.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
-            }
-
-            if (item is TabItem tabItem)
-                ((TabControl)tabItem.Parent)?.Items.Remove(tabItem);
-            itemsSource.Insert(newIndex, item);
-
-            VisualHelper.DoEvents(DispatcherPriority.Render);
-
-            element = control.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
-            if (element != null)
-            {
-                // element.SetCurrentValueSmart(UIElement.OpacityProperty, 1.0);
-                await Task.WhenAll(AnimationHelper.GetHeightContentAnimations(element, true));
-                element.SetCurrentValueSmart(FrameworkElement.HeightProperty, double.NaN);
-            }
-
-            control.ItemContainerGenerator.StatusChanged -= OnItemContainerGeneratorStatusChanged;
-
-            void OnItemContainerGeneratorStatusChanged(object sender, EventArgs e)
-            {
-                var generator = (ItemContainerGenerator)sender;
-                if (generator.Status != GeneratorStatus.ContainersGenerated) return;
-                foreach (var item2 in generator.Items)
-                    if (generator.ContainerFromItem(item2) is FrameworkElement fe && fe.Opacity > double.Epsilon && fe.ActualHeight < double.Epsilon && fe.ActualWidth < double.Epsilon)
-                        fe.SetCurrentValueSmart(UIElement.OpacityProperty, 0.0);
             }
         }
 
