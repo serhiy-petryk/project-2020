49d3088c8417892964c80ee5f4135139580fc906
serhiy-petryk <sergei_petrik@yahoo.com>
2022-04-25 21:46:38 +0300

    STARTED FormatBox

---
+++ DG.4.0/WpfSpLib/Controls/FormatBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfSpLib.Controls
+{
+    public class FormatBox: Control//, INotifyPropertyChanged
+    {
+        public string LanguageChangeHook
+        {
+            get
+            {
+                InternalSetText(Value);
+                return null;
+            }
+        }
+
+        private void InternalSetText(string newValue)
+        {
+            /*if (_textBox == null) return;
+            if (newValue.HasValue)
+                _textBox.Text = FormattedValue(newValue, StringFormat, Culture);
+            else
+                _textBox.Text = null;*/
+        }
+
+        //=============================
+        // public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value",
+          //  typeof(string), typeof(FormatBox), new FrameworkPropertyMetadata("F2", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnValueChanged, CoerceValue));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value",
+            typeof(string), typeof(FormatBox), new FrameworkPropertyMetadata("F2"));
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+    }
+}
