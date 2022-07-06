07e2bbf9ef187eb52d2b4b3401c6fb2d279396d9
serhiy-petryk <sergei_petrik@yahoo.com>
2022-07-07 00:41:37 +0300

    moved JsonTimeSpanConverter to Utils.Json.cs

--- DG.4.0/DGCore/Utils/Json.cs
+++ DG.4.0/DGCore/Utils/Json.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using DGCore.Helpers;
 
 namespace DGCore.Utils
 {
@@ -97,5 +97,15 @@ namespace DGCore.Utils
                     ConvertJsonElements(propValue);
             }
         }
+
+        public class JsonTimeSpanConverter : JsonConverter<TimeSpan>
+        {
+            public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+                TimeSpan.Parse(reader.GetString(), CultureInfo.InvariantCulture);
+
+            public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options) =>
+                writer.WriteStringValue(value.ToString("g", CultureInfo.InvariantCulture));
+        }
+
     }
 }
