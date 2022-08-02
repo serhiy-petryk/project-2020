682a4c9e94be6e357c8eef56cb287508177eb76f
serhiy-petryk <sergei_petrik@yahoo.com>
2022-08-02 15:38:01 +0300

    fixed scroll bars in DGWnd.UI.frmDependentObjectManager

--- DG.4.0/DGWnd/UI/frmDependentObjectManager.Designer.cs
+++ DG.4.0/DGWnd/UI/frmDependentObjectManager.Designer.cs
@@ -23,32 +23,35 @@ namespace DGWnd.UI {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-      this.txtLog = new System.Windows.Forms.TextBox();
-      this.SuspendLayout();
-      // 
-      // txtLog
-      // 
-      this.txtLog.BackColor = System.Drawing.Color.White;
-      this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.txtLog.Location = new System.Drawing.Point(0, 0);
-      this.txtLog.Multiline = true;
-      this.txtLog.Name = "txtLog";
-      this.txtLog.ReadOnly = true;
-      this.txtLog.Size = new System.Drawing.Size(402, 505);
-      this.txtLog.TabIndex = 0;
-      // 
-      // frmDependentObjectManager
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(402, 505);
-      this.Controls.Add(this.txtLog);
-      this.Name = "frmDependentObjectManager";
-      this.Text = "Залежні обєкти";
-      this.Load += new System.EventHandler(this.frmDependentObjectManager_Load);
-      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmDependentObjectManager_FormClosed);
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.Color.White;
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(469, 583);
+            this.txtLog.TabIndex = 0;
+            // 
+            // frmDependentObjectManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 583);
+            this.Controls.Add(this.txtLog);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmDependentObjectManager";
+            this.Text = "Залежні обєкти";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmDependentObjectManager_FormClosed);
+            this.Load += new System.EventHandler(this.frmDependentObjectManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
