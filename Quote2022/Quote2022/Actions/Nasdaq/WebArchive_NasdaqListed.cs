612429521ac16e73d32a47cd6af2e5f89a720797
serhiy-petryk <sergei_petrik@yahoo.com>
2023-03-07 00:09:29 +0200

    Quote2022

---
+++ Quote2022/Quote2022/Actions/Nasdaq/WebArchive_NasdaqListed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Quote2022.Actions.Nasdaq
+{
+    public static class WebArchive_NasdaqListed
+    {
+        #region =============  Download Html Data  ===============
+        public static void DownloadHtmlData(Action<string> showStatusAction)
+        {
+            var url = @"http://www.nasdaqtrader.com/dynamic/symdir/nasdaqlisted.txt";
+            showStatusAction($"WebArchive_NasdaqListed finished");
+        }
+
+        public class cYearList
+        {
+            public Dictionary<string, object> status;
+            public string first_ts;
+            public string last_ts;
+        }
+        public class cDayList
+        {
+            public int[][] items;
+        }
+        #endregion
+
+    }
+}
