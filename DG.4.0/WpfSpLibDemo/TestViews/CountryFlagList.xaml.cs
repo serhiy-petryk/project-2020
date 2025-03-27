0d74ab1f0a9b8ee0c08bd94c976e5e2e02361302
serhiy-petryk <sergei_petrik@yahoo.com>
2025-03-28 01:07:03 +0200

    refactored CountryFlagList

--- DG.4.0/WpfSpLibDemo/TestViews/CountryFlagList.xaml.cs
+++ DG.4.0/WpfSpLibDemo/TestViews/CountryFlagList.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfSpLibDemo.TestViews
 {
@@ -19,9 +13,34 @@ namespace WpfSpLibDemo.TestViews
     /// </summary>
     public partial class CountryFlagList : Window
     {
+
+        public class FlagItem
+        {
+            public string Name { get; set; }
+            public ImageSource Image { get; set; }
+        }
+
+        public List<FlagItem> FlagItems = new List<FlagItem>();
+
         public CountryFlagList()
         {
             InitializeComponent();
+
+            var a1 = Application.Current.Resources["RegionIcon_CA-ES"];
+            var aa1 = Application.Current.Resources;
+            var aa2 = aa1.Keys;
+
+            foreach (var dict in Resources.MergedDictionaries)
+            foreach (DictionaryEntry entry in dict)
+            {
+                var key = entry.Key as string;
+                var image = entry.Value as ImageSource;
+                if (image != null && (key.StartsWith("Region", StringComparison.CurrentCultureIgnoreCase) ||
+                                      key.StartsWith("Country", StringComparison.CurrentCultureIgnoreCase)))
+                    FlagItems.Add(new FlagItem { Name = key, Image = image });
+            }
+
+            ImageList.ItemsSource = FlagItems;
         }
     }
 }
