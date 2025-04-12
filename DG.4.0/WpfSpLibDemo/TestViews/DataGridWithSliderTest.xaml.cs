669761dbc1ad32a8399935f701bd19322a80f1cd
serhiy-petryk <sergei_petrik@yahoo.com>
2025-04-12 06:55:40 +0300

    x

--- DG.4.0/WpfSpLibDemo/TestViews/DataGridWithSliderTest.xaml.cs
+++ DG.4.0/WpfSpLibDemo/TestViews/DataGridWithSliderTest.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
 using WpfSpLib.Common.ColorSpaces;
 using WpfSpLib.Helpers;
 using WpfSpLibDemo.Samples;
@@ -44,10 +48,18 @@ namespace WpfSpLibDemo.TestViews
             BaseHsl2 = new HSL_Observable() { Hue = hsl2.Hue, Saturation = hsl2.Saturation, Lightness = hsl2.Lightness };
         }
 
+
+        private void ResetSlider()
+        {
+            DGSlider.Maximum = Data.Count - 1;
+            DGSlider.Value = 0;
+        }
+
         private void BtnGenerate_OnClick(object sender, RoutedEventArgs e)
         {
             var cnt = Convert.ToInt32(ItemCount.Text);
             FakeData.GenerateData(Data, cnt);
+            ResetSlider();
         }
 
         private void Grid_OnLoadingRow(object sender, DataGridRowEventArgs e)
@@ -73,6 +85,51 @@ namespace WpfSpLibDemo.TestViews
         {
             var hsl = new HSL(new RGB(0xF5 / 256.0, 0xFA / 256.0, 0xFF / 256.0));//FFB0C4DE
             BaseHsl = new HSL_Observable() { Hue = hsl.Hue, Saturation = hsl.Saturation, Lightness = hsl.Lightness };
+        }
+
+
+        private int _lastSlidedRowNo = -1;
+        private object _scrollLock = new object();
+        private void DGSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            System.Threading.Monitor.Enter(_scrollLock);
+            try
+            {
+                if (!_isSliderBusy)
+                    Slide();
+            }
+            finally
+            {
+                System.Threading.Monitor.Exit(_scrollLock);
+            }
+            //throw new NotImplementedException();
+            var a1 = Convert.ToInt32(DGSlider.Value);
+            Grid1.ScrollIntoView(Data[a1]);
+
+            var aa1 = Grid1.GetVisualChildren().OfType<ScrollViewer>().ToArray();
+            var aa2 = Grid1.GetVisualChildren().OfType<ScrollBar>().Where(a=>a.Orientation == Orientation.Vertical).ToArray()[0];
+            // aa2.Value += 1000;
+        }
+
+        private bool _isSliderBusy = false;
+        private void Slide()
+        {
+            _isSliderBusy = true;
+            var rowNo = Convert.ToInt32(DGSlider.Value);
+            if (rowNo != _lastSlidedRowNo)
+            {
+                Debug.Print($"Slide: {rowNo} {DateTime.Now}");
+                Grid1.ScrollIntoView(Data[rowNo]);
+                Grid1.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        _lastSlidedRowNo = rowNo;
+                        _isSliderBusy = false; 
+                        Slide();
+                    }),
+                    DispatcherPriority.Render);
+            }
+            else
+                _isSliderBusy = false;
         }
     }
 }
