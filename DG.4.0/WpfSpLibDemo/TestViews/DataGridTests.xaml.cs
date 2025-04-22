5a79bb056747535fa0237a4f739fc6e92c2c2ffc
serhiy-petryk <sergei_petrik@yahoo.com>
2025-04-22 10:02:19 +0300

    added ColorPicker to DataGridTests form

--- DG.4.0/WpfSpLibDemo/TestViews/DataGridTests.xaml.cs
+++ DG.4.0/WpfSpLibDemo/TestViews/DataGridTests.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using WpfSpLib.Common.ColorSpaces;
 using WpfSpLib.Helpers;
 using WpfSpLibDemo.Samples;
 
@@ -12,6 +13,16 @@ namespace WpfSpLibDemo.TestViews
     /// </summary>
     public partial class DataGridTests : Window
     {
+        #region ============== Properties/Events  ===================
+        public static readonly DependencyProperty BaseHslProperty = DependencyProperty.Register("BaseHsl",
+            typeof(HSL_Observable), typeof(DataGridTests), new FrameworkPropertyMetadata(null));
+        public HSL_Observable BaseHsl
+        {
+            get => (HSL_Observable)GetValue(BaseHslProperty);
+            set => SetValue(BaseHslProperty, value);
+        }
+        #endregion
+
         public IList<Author> Data { get; } = Author.Authors;
         public Author.Level[] EnumList { get; } = Enum.GetValues<Author.Level>();
 
@@ -19,9 +30,19 @@ namespace WpfSpLibDemo.TestViews
         {
             InitializeComponent();
             DataContext = this;
+            var xhsl = new HSL(new RGB(0xB0 / 256.0, 0xC4 / 256.0, 0xDE / 256.0));//FFB0C4DE
+            var hsl = new HSL(new RGB(0x78 / 256.0, 0xBC / 256.0, 0xFF / 256.0));//FF78BCFF
+            BaseHsl = new HSL_Observable() { Hue = hsl.Hue, Saturation = hsl.Saturation, Lightness = hsl.Lightness };
         }
 
         private void DataGrid_OnThreeStateSorting(object sender, DataGridSortingEventArgs e) =>
             DataGridHelper.DataGrid_OnSorting((DataGrid)sender, e);
+
+        private void ChangeHsl_OnClick(object sender, RoutedEventArgs e)
+        {
+            var hsl = BaseHsl;
+            var a = (hsl.Hue + 30.0) % 360;
+            hsl.Hue = a;
+        }
     }
 }
