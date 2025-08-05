b4713b359e4497f414d924dd908cf3ba473b51e9
serhiy-petryk <sergei_petrik@yahoo.com>
2025-08-05 03:47:55 +0300

    fixed RippleEffectTests

--- DG.4.0/WpfSpLibDemo/TestViews/RippleEffectTests.xaml.cs
+++ DG.4.0/WpfSpLibDemo/TestViews/RippleEffectTests.xaml.cs
@@ -1,14 +1,25 @@
 ﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using WpfSpLib.Helpers;
+using WpfSpLib.Themes;
 
 namespace WpfSpLibDemo.TestViews
 {
     /// <summary>
     /// Interaction logic for RippleEffectTests.xaml
     /// </summary>
-    public partial class RippleEffectTests : Window
+    public partial class RippleEffectTests : Window, IColorThemeSupport
     {
+        #region ========  IColorThemeSupport ==========
+        public MwiThemeInfo Theme { get; set; }
+        public Color? ThemeColor { get; set; }
+        public MwiThemeInfo ActualTheme { get; }
+        public Color ActualThemeColor => Colors.White;
+        public IColorThemeSupport ColorThemeParent { get; }
+        #endregion
+
         public RippleEffectTests()
         {
             InitializeComponent();
